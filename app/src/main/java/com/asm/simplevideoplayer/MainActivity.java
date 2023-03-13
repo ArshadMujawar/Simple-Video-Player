050af8e2124b5fc0e2fae91f37ce104437fe3d0a
Arshad <arshadmujawar15@gmail.com>
2023-03-13 08:53:15 -0400

    Monday Normal Changes

---
+++ app/src/main/java/com/asm/simplevideoplayer/MainActivity.java
@@ -0,0 +1,33 @@
+package com.asm.simplevideoplayer;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final int REQUEST_PERMISSION_SETTING = 12;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+            Toast.makeText(this, "Click on Permission and allow storage", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+            Uri uri = Uri.fromParts("package", getPackageName(), null);
+            intent.setData(uri);
+            startActivityForResult(intent,REQUEST_PERMISSION_SETTING);
+        }
+    }
+}
---
+++ app/src/main/java/com/asm/simplevideoplayer/VideoFoldersAdapter.java
@@ -0,0 +1,5 @@
+package com.asm.simplevideoplayer;
+
+public class VideoFoldersAdapter {
+
+}
